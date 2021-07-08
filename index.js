fee943127de19c0003422d3fb6d13d5117b8fd12
Anubhav2907 <u19ec135@eced.svnit.ac.in>
2021-07-08 23:40:35 +0530

    initial

---
+++ index.js
@@ -0,0 +1,49 @@
+const express = require("express");
+const path = require("path");
+const mongoose = require("mongoose");
+const session = require("express-session");
+const app = express();
+const bcrypt = require("bcryptjs");
+const DBURL =
+  "mongodb+srv://Anubhav:trip@cluster0.m7sr4.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+
+var cookieParser = require("cookie-parser");
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, "public")));
+const sessionconfig = {
+  name: "session",
+  secret: "mysecret",
+  resave: false,
+  saveUninitialized: true,
+  cookie: {
+    expires: Date.now() + 1000 * 60 * 60 * 24 * 7,
+    maxAge: 1000 * 60 * 60 * 24 * 7,
+  },
+};
+app.use(session(sessionconfig));
+app.use(cookieParser());
+app.use(function (req, res, next) {
+  res.locals.currentUser = req.user;
+  next();
+});
+
+mongoose.connect(DBURL, {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useUnifiedTopology: true,
+  useFindAndModify: false,
+});
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "Connection error:"));
+db.once("open", function () {
+  console.log("Database Connected");
+});
+
+app.get("/", function (req, res) {
+  res.send("home");
+});
+
+const port = process.env.PORT || 3000;
+app.listen(port, function () {
+  console.log(`On port ${port}`);
+});
